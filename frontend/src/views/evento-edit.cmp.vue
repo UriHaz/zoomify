128b427794c8c09ca64e727560215f8961d70e50
Naama Shoshani <naamashoshani@gmail.com>
2020-07-23 01:24:06 +0300

    01:24

--- frontend/src/views/evento-edit.cmp.vue
+++ frontend/src/views/evento-edit.cmp.vue
@@ -9,15 +9,23 @@
         <h3>My event is about...</h3>
         <input class="add-event-placeholder" type="text" v-model="eventoToEdit.title" placeholder="Add event title..." />
       </label>
+
+
+
+      <div class="event-date-time">
       <label>
-        <h3>Date! Be ready on...</h3>
+        <h3>Date! Be there on:</h3>
         <!-- <input class="add-event-placeholder" type="date" v-model="eventoToEdit.startDate" placeholder="Event start date..." /> -->
-        <input class="add-event-placeholder" type="date" v-model="eventoToEdit.startDate" />
+        <input class="add-event-date" type="date" v-model="eventoToEdit.startDate" />
       </label>
       <label>
-        <h3>Time! Starting exactly at...</h3>
-        <input class="add-event-placeholder" type="time" v-model="eventoToEdit.startTime" />
+        <h3>Time! Starting at:</h3>
+        <input class="add-event-time" type="time" v-model="eventoToEdit.startTime" />
       </label>
+      </div>
+
+
+
 
       <label>
         <h3>Event Description:</h3>
