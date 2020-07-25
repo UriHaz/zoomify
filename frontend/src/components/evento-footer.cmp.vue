5d88e2c9b18ff4b545639f5a67e28453a227f8d8
Uri Hazan <rutuforlife@gmail.com>
2020-07-25 22:53:29 +0300

    dfdf

--- frontend/src/components/evento-footer.cmp.vue
+++ frontend/src/components/evento-footer.cmp.vue
@@ -1,6 +1,8 @@
 <template>
  <section class="evento-footer">
-   <div></div>
+   <div>
+    <router-link to="/"> <img class="logo-img" src="../assets/imgs/eyes logo.png"/></router-link>
+   </div>
  </section>
 </template>
 
