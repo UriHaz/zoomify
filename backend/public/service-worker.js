0e2a77eef5ec647cc38b23da7236248012473b5e
Uri Hazan <rutuforlife@gmail.com>
2020-07-26 11:03:41 +0300

    dfgf

--- backend/public/service-worker.js
+++ backend/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.61524020d2b6d2543c5ae41a1c667a1f.js"
+  "/precache-manifest.81942687b19ecdc1860e078c3a3a9588.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend"});
