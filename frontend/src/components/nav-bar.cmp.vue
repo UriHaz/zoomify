5d6bd8f6e57df81f2b7a9d85c55adb5e54262be7
Uri Hazan <rutuforlife@gmail.com>
2020-07-27 19:04:16 +0300

    Dfdf

--- frontend/src/components/nav-bar.cmp.vue
+++ frontend/src/components/nav-bar.cmp.vue
@@ -1,21 +1,23 @@
 <template>
-  <nav class="nav-bar">
-    <router-link to="/"> <img class="logo-img" src="../assets/imgs/eyes logo.png"/></router-link>
-    <div class="nav-links">
-      <router-link to="/events">Events</router-link>
-      <router-link to="/event/edit">Create Event</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link v-if="!loggedInUser" to="/login">Login</router-link>
-      <router-link v-if="loggedInUser" to="/login">
-      <button v-if="loggedInUser" class="user-btn">
-      <avatar v-if="!loggedInUser.imgUrl" class="nav-user-img" inline :size="30" :username="loggedInUser.fullName"></avatar>
-      <avatar v-else class="nav-user-img" inline :size="30" :src="loggedInUser.imgUrl"></avatar>
-      {{loggedInUser.fullName}}
-      </button>
-      </router-link>
-      <!-- <button class="btn-menu" @click="toggleMenu">☰</button> -->
-    </div>
-  </nav>
+  <section class="nav-bar-container">
+    <nav class="nav-bar">
+      <router-link to="/"> <img class="logo-img" src="../assets/imgs/eyes logo.png"/></router-link>
+      <div class="nav-links">
+        <router-link to="/events">Events</router-link>
+        <router-link to="/event/edit">Create Event</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link v-if="!loggedInUser" to="/login">Login</router-link>
+        <router-link v-if="loggedInUser" to="/login">
+        <button v-if="loggedInUser" class="user-btn">
+        <avatar v-if="!loggedInUser.imgUrl" class="nav-user-img" inline :size="30" :username="loggedInUser.fullName"></avatar>
+        <avatar v-else class="nav-user-img" inline :size="30" :src="loggedInUser.imgUrl"></avatar>
+        {{loggedInUser.fullName}}
+        </button>
+        </router-link>
+        <!-- <button class="btn-menu" @click="toggleMenu">☰</button> -->
+      </div>
+    </nav>
+  </section>
 </template>
 
 
